b2171dbc319cc9fda31e4922898166826d78c752
WANCZER <ravishant250@gmail.com>
2026-07-16 23:33:31 +0530

    Cleanup deployment script

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import os
+import sys
+
+# Ensure root folder is in python path for serverless imports
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
